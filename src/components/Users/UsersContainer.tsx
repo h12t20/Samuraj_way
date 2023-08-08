e8ff8322b40d011112bbe4bde2a9ce4b2799b452
h12t20 <sergdiamond@ymail.com>
2023-08-08 13:09:52 +0300

    add pagination, refactoring

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -1,10 +1,42 @@
+import React, {ReactNode} from "react";
 import {connect} from "react-redux";
-import {UsersAPI} from './UsersAPI';
-import {ActionType, StateType} from "../../Redux/redux_store";
-import {
-    followAC, setCurrentPageAC, setTotalUsersCountAC, setUserAC,
-    UserType
+import {ActionType, StateType, UsersType} from "../../Redux/redux_store";
+import {followAC, setCurrentPageAC, setTotalUsersCountAC, setUserAC, UserType
 } from "../../Redux/users_reducer";
+import axios from "axios";
+import {Users} from "./Users";
+type UsersPropsType = {
+    usersPage: UsersType;
+    follow: (userId: number) => void;
+    setUsers: (users: UserType[]) => void,
+    setCurrentPage: (page: number) => void,
+    setTotalUsersCount: (totalUsers: number) => void
+}
+
+export class UsersAPI extends React.Component<UsersPropsType, {children?: ReactNode}> {
+    componentDidMount() {
+        axios
+            .get(`https://social-network.samuraijs.com/api/1.0//users?page=${this.props.usersPage.currentPage}&count=${this.props.usersPage.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount)
+            })
+    }
+
+    onPageChanged = (page: number) => {
+        this.props.setCurrentPage(page);
+        axios
+            .get(`https://social-network.samuraijs.com/api/1.0//users?page=${page}&count=${this.props.usersPage.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+            })
+    }
+
+    render() {
+        return <Users usersPage={this.props.usersPage} follow={this.props.follow} onPageChanged={this.onPageChanged}/>
+    }
+}
+
 
 const mapStateToProps = (state: StateType) => {
     return {
