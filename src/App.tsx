35491b6a95e9dbfed2a0e1e242b2b509a9a098b9
h12t20 <sergdiamond@ymail.com>
2023-09-24 10:53:55 +0300

    add lazily, suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {ReactNode, Suspense} from "react";
+import React, {ComponentType, lazy, ReactNode, Suspense} from "react";
 import './App.css'
 import {Navbar} from "./components/Navbar/Navbar";
 import {Route} from "react-router-dom";
@@ -11,35 +11,51 @@ import {connect} from "react-redux";
 import {initializeApp} from "./Redux/app_reducer";
 import {StateType} from "./Redux/redux_store";
 import {Preloader} from "./components/common/Preloader";
-import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import {lazily} from "react-lazily";
-const {DialogsContainer} = lazily(()=>import("./components/Dialogs/DialogsContainer"));
-class App extends React.Component<{ initializeApp:()=>void, initApp:boolean }, { children?: ReactNode }>  {
+
+const {DialogsContainer} = lazily(() => import("./components/Dialogs/DialogsContainer"));
+
+const ProfileContainer = lazy(() => import("./components/Profile/ProfileContainer")
+    .then((ProfileContainer) => ProfileContainer as { default: ComponentType<any> }));
+
+const UsersContainer = lazy(() => import("./components/Users/UsersContainer")
+    .then((UsersContainer) => UsersContainer as { default: ComponentType<any> }));
+class App extends React.Component<{
+    initializeApp: () => void,
+    initApp: boolean
+}, {
+    children?: ReactNode
+}> {
     componentDidMount() {
         this.props.initializeApp()
     }
+
     render() {
-        if (!this.props.initApp) { return <Preloader/>}
+        if (!this.props.initApp) {
+            return <Preloader/>
+        }
+        // @ts-ignore
+        // @ts-ignore
         return (
             <div className='app-wrapper'>
                 <HeaderContainer/>
                 <Navbar/>
                 <div className='app-wrapper-content'>
-                    <Route exact path='/'><ProfileContainer/></Route>
-                    <Route path='/news'><News/></Route>
-                    <Route path='/music'><Music/></Route>
-                    <Route path='/profile/:id?'><Suspense fallback={<Preloader/>}>
-                        <ProfileContainer/></Suspense></Route>
-                    <Route path='/dialogs'><Suspense fallback={<Preloader/>}>
-                        <DialogsContainer/></Suspense></Route>
-                    <Route path='/settings'><Settings/></Route>
-                    <Route path='/users'><UsersContainer/></Route>
-                    <Route path='/login'><Login/></Route>
+                    <Suspense fallback={<div><Preloader/></div>}>
+                        <Route exact path='/'><ProfileContainer/></Route>
+                        <Route path='/news'><News/></Route>
+                        <Route path='/music'><Music/></Route>
+                        <Route path='/profile/:id?'><ProfileContainer/></Route>
+                        <Route path='/dialogs'><DialogsContainer/></Route>
+                        <Route path='/settings'><Settings/></Route>
+                        <Route path='/users'><UsersContainer/></Route>
+                        <Route path='/login'><Login/></Route>
+                    </Suspense>
                 </div>
             </div>
         )
     }
 }
-const mapStateToProps = (state: StateType) => ({initApp:state.app.initialized});
+
+const mapStateToProps = (state: StateType) => ({initApp: state.app.initialized});
 export default connect(mapStateToProps, {initializeApp})(App)
